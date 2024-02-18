32f4712cbe9123ac43063dc80bc96c61b7952bbc
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-18 17:37:30 +0530

    Add project files.

---
+++ JoesPizzaWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoesPizzaWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
